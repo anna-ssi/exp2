d6734ef0c6eb36d48f8048618ee8b795a1b2272f
anna-ssi <annahakhverdyan98@gmail.com>
2023-11-28 00:16:46 -0700

    added scripts for each net

--- scripts/seq.sh
+++ scripts/seq.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=seq
+#SBATCH --job-name=seq_aug
 #SBATCH --account=rrg-whitem
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=3
 #SBATCH --time=0-02:55
 #SBATCH --mem=16G
-#SBATCH -o ./seq.out
+#SBATCH -o ./results/seq_aug.out
 
 module load python/3.10.2
 source ./venv/bin/activate
